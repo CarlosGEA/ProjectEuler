3de691dddb88a930f63753e3ffdc03398e60a86f
CarlosGEA <carlosgea31@gmail.com>
2024-06-02 23:58:52 +0100

    3 more challenges

---
+++ Challenge12.py
@@ -0,0 +1,43 @@
+"""
+Name : Highly Divisible Triangular Number
+Date created : 02-06-2024
+"""
+
+import math
+
+TRIAL = 5
+CHALLENGE = 500
+
+
+def factors(num):
+
+    total = 0
+    for i in range(1, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            total += 1
+    total *= 2
+    if num == i * i:
+        total -= 1
+    return total
+
+
+def main():
+
+    number = 0
+    triangle_number = 0
+    count = 1
+
+    while number <= CHALLENGE:
+        triangle_number += count
+        number = factors(triangle_number)
+        # if number > 200:
+        #     print(triangle_number, number)
+        count += 1
+
+
+    print(f"The first triangle number to have over {CHALLENGE} divisors is {triangle_number}")
+    return None
+
+
+if __name__ == "__main__":
+    main()
