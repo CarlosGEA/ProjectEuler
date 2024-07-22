b241e4a9bcd71ab364059d6faf9f4072bc4bbf04
CarlosGEA <carlosgea31@gmail.com>
2024-07-22 17:14:15 +0100

    c45+46

--- 0045.py
+++ 0045.py
@@ -3,8 +3,43 @@ Name : Triangular, Pentagonal, and Hexagonal
 Date created : 22-07-2024
 """
 
+import math
+import itertools
+
+
+# def generateTriangle():
+#     t = 1
+#     d = 1
+#     while True:
+#         yield t
+#         t += d
+#         d += 1
+
+
+def generateTriangle(n):
+    return int(n * (n + 1) / 2)
+
+
+def isPentagonal(num):
+    return math.isqrt(1 + 24 * num) and ((1 + math.sqrt(1 + 24 * num)) % 6 == 0)
+
+
+def isHexagonal(num):
+    return math.isqrt(1 + 8 * num) and ((1 + math.sqrt(1 + 8 * num)) % 4 == 0)
+
+
 def main():
-   
+    # for p in itertools.islice(generateTriangle(), 10):
+    #     print(p)
+    tph = 0
+    n = 1
+    while tph <= 40755:
+        num = generateTriangle(n)
+        if isPentagonal(num) and isHexagonal(num):
+            tph = num
+        n += 1
+    print(num)
+
     return None
 
 
---
+++ 0046.py
@@ -0,0 +1,39 @@
+"""
+Name : 
+Date created : 22-07-2024
+"""
+
+import math
+
+
+def isPrime(n):
+    if n < 2:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    for x in range(6, math.floor(math.sqrt(n)) + 2, 6):
+        if n % (x - 1) == 0 or n % (x + 1) == 0:
+            return False
+    return True
+
+
+def main():
+    double_square = [2 * n * n for n in range(1, 30)]
+    possible_nums = set()
+    scn = 0
+    n = 1
+    while not scn:
+        if isPrime(n):
+            for sq in double_square:
+                possible_nums.add(sq + n)
+        elif n % 2 != 0 and n not in possible_nums and n != 1:
+            scn = n
+        n += 1
+    print(f"The smallest composite number not finding condition is {scn}")
+    return None
+
+
+if __name__ == "__main__":
+    main()
